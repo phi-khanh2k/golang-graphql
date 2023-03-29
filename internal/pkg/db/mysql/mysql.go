40103e3ae18fe946bc84cb2e8f0b6fc98da40773
duyvu997 <ngocduy0997@gmail.com>
2023-03-29 09:36:28 +0700

    fix db

--- internal/pkg/db/mysql/mysql.go
+++ internal/pkg/db/mysql/mysql.go
@@ -12,7 +12,7 @@ import (
 var Db *sql.DB
 
 func InitDB() {
-	db, err := sql.Open("mysql", "root:dbpass@(172.17.0.2:3306)/hackernews")
+	db, err := sql.Open("mysql", "root:dbpass@(127.0.0.1:3306)/hackernews")
 	if err != nil {
 		log.Panic(err)
 	}
